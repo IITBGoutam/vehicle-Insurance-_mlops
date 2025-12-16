ff140dc1b8958c7d55c91f17b01cc3a8f61be0a4
Goutam <goutamsinghisreal@gmail.com>
2025-12-16 14:19:00 +0530

    basic structure ready

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="Goutam Singh",
+    author_email="23b2428@iitb.ac.in",
+    packages=find_packages()
+)
